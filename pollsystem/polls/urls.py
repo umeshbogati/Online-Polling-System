eaabee394922fa99816003c4dc66bf41f45c703b
umeshbogati <bogatiu17@gmail.com>
2026-01-02 21:29:19 +0545

    I have work on urls where i give url path for Auth.

--- pollsystem/polls/urls.py
+++ pollsystem/polls/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+
     path('', views.poll_list, name='poll_list'),
     path('poll/<int:poll_id>/', views.poll_detail, name='poll_detail'),
     path('vote/<int:poll_id>/', views.vote, name='vote'),
     path('result/<int:poll_id>/', views.result, name='result'),
-    path('login/', views.user_login, name='login'),
-]
+    
+] 
