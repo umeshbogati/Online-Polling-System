706283df962be27c21d1fb0125403aa16d9fd043
umeshbogati <bogatiu17@gmail.com>
2026-01-02 21:11:53 +0545

    I changes lits bit code.

--- pollsystem/pollsystem/settings.py
+++ pollsystem/pollsystem/settings.py
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.debug',
             ],
         },
     },
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_URL = '/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
